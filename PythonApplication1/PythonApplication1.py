5e276b4be28f430797fe14170415f8ad9841962c
TheSpecial <Ignatiev.alex2015@yandex.ru>
2022-06-02 22:31:01 +0300

    Базовая работа с файлами

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,14 +1,9 @@
 
-mass = [1,5,9,7,3,6,4,8,2,651,22,8181,9999,1,0,1]
-end = len(mass)
-swaps = True
-while end > 1 or swaps:
-    end = max(1, int(end / 1.25))  # minimum end is 1
-    swaps = False
-    for i in range(len(mass) - end):
-        j = i + end
-        if mass[i] > mass[j]:
-            mass[i], mass[j] = mass[j], mass[i]
-            swaps = True
+arr = []
+with open('Test.txt','r+') as f:
+    arr = [row.strip('.,!?') for row in f.read().split()]
+print(arr)
 
-print (mass)
+with open('TestOutput.txt','w') as ff:
+    for j in range(len(arr)):
+        ff.writelines(arr[j] + '\n')
