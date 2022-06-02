f156422b80c7ff1246aeffd8098b8cbbe8bc2302
TheSpecial <Ignatiev.alex2015@yandex.ru>
2022-06-02 22:36:44 +0300

    Ввод текста и его сортировка

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,9 +1,28 @@
 
-arr = []
-with open('Test.txt','r+') as f:
-    arr = [row.strip('.,!?') for row in f.read().split()]
-print(arr)
+import time # Библиотека для замера времени (очевидно)
 
-with open('TestOutput.txt','w') as ff:
-    for j in range(len(arr)):
-        ff.writelines(arr[j] + '\n')
+def sort(arr): #Сортировка листа (текста) по возрастанию "расчёской"
+    totaltime = 0
+    for j in range (1000): #1000 повторений для точного определения времени. 1000 работает довольно долго, уменьшите по необходимости
+        start_time = time.time()
+        end = len(arr)
+        swaps = True
+        while end > 1 or swaps:
+            end = max(1, int(end / 1.247))
+            swaps = False
+            for i in range(len(arr) - end):
+                j = i + end
+                if arr[i].lower() > arr[j].lower(): # Использую lower чтобы не учитывать регистр, когда сортирую по возрастанию
+                    arr[i], arr[j] = arr[j], arr[i] # А вот меняю местами не lower, чтобы сохранить регистр при выводе
+                    swaps = True
+        ttime = time.time() - start_time
+        totaltime += ttime #Время за 1000 раз
+    return[(totaltime/1000)] #Поэтому и делю на 1000
+
+
+with open('F:/Practics/Input/Original1.txt','r+') as f: # Бояре не закрывают файлы, они используют with
+    arrraw = [row.strip() for row in f.read().split()] # Сырой лист(массив)
+    arr = [row.strip('.,!?—:;()«»-IV1234567890') for row in arrraw] # без знаков препинания и тп.
+    while ('' in arr): # Удаление пустых ячеек. Из-за удаления знаков препинания некоторые ячейки стали пустыми. Чтобы верно определить количество слов и работать с листом(массивом) нужно их удалить
+            arr.remove('')
+ttime = sort(arr)
